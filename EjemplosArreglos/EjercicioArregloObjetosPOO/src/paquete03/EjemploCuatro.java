7006e74adc6531df75f94ebbb338f3138f95cb20
Byron Alvarez <bvalvarez1.1@gmail.com>
2024-05-15 16:45:34 -0500

    cambios

--- EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/EjemploCuatro.java
+++ EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/EjemploCuatro.java
@@ -8,7 +8,7 @@ package paquete03;
 import paquete01.Calificacion;
 import paquete01.Profesor;
 
-public class Ejemplo {
+public class EjemploCuatro {
     public static void main(String[] args) {
         
         Profesor profesor1 = new Profesor("Franco", "nombramiento");
@@ -17,18 +17,23 @@ public class Ejemplo {
         // crear un arreglo de objetos de tipo Calificacion
         Calificacion [] calificaciones = new Calificacion[2];
         
-        Calificacion c = new Calificacion(10, "Computación");
-        Calificacion c2 = new Calificacion(9, "Electrónica");
+        Calificacion c = new Calificacion(10, "Computación", profesor1);
+        Calificacion c2 = new Calificacion(9, "Electrónica", profesor2);
         
         calificaciones[0] = c;
         calificaciones[1] = c2;
         
         for (int i = 0; i < calificaciones.length; i++) {
             Calificacion objetoCalificacion = calificaciones[i];
-            System.out.printf("%s - %.2f\n", 
+            System.out.printf("%s - %.2f - Profesor(%s)\n", 
                     objetoCalificacion.obtenerNombreMateria(),
-                    objetoCalificacion.obtenerNota());
+                    objetoCalificacion.obtenerNota(),
+                    objetoCalificacion.obtenerProfesor().obtenerNombre());
         }
+        /*
+        En la linea final, llamamos al objetoCalificacion, al metodo 
+        obtenerProfesor y al metodo de ese objeto, obtenerNombre
+        */
         
     }
 }
