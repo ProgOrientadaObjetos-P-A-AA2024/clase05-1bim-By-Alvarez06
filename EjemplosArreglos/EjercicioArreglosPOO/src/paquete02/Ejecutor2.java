6905ce152dfab74d254d96e5a9c4ef592804f839
Byron Alvarez <bvalvarez1.1@gmail.com>
2024-05-15 16:08:37 -0500

    cambios

--- EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
+++ EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
@@ -9,7 +9,20 @@ public class Ejecutor2 {
         String nombreEstudiante = "René Elizalde";
         LibretaCalificacion libreta1 = new LibretaCalificacion(nombreEstudiante, 
                 notas);
+        
+        /*
+        Si se deja comentada la linea en la cual llama al metodo establecer
+        Promedio, al nunca llamar a ese método el valor que va a presentar por
+        pantalla va a ser igual al valor por defecto de una variable de numero
+        es decir, 0
+        */
         libreta1.establecerPromedio();
+        /*
+        Por otro lado, si se deja comentada la linea en la cual llama al metodo
+        estalecerPromedioCualitativo, al nunca llamar a ese método el valor que
+        va a presentar por pantalla va a ser igual al valor por defecto de una
+        variable de tipo cadena, es decir, null
+        */
         libreta1.establecerPromedioCualitativo();
 
         System.out.printf("\n%s\n", libreta1);
